1547c66ea7b5e52ef94ea4289f01b156a8a1e71f
pleasehiremesrs <thebilliestbob@gmail.com>
2025-03-30 04:39:39 -0400

    added my super cool script

---
+++ track_batman.py
@@ -0,0 +1,7 @@
+import random
+
+# My very special data that I almost died collecting
+longitude = [1, 2, 3, 4, 5]
+latitude = [6, 7, 8, 9, 10]
+
+print("Batman will most likely be at these coordinates tonight: (%d, %d)" % (random.choice(longitude), random.choice(latitude)))
